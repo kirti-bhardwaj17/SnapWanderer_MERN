9e0ef77523827e61d2479a71e36c8f18860b12c1
Kirti Bhardwaj <106263358+kirti-bhardwaj17@users.noreply.github.com>
2024-11-25 16:58:34 +0530

    Update BlogDetail.js

--- frontend/src/components/BlogDetail.js
+++ frontend/src/components/BlogDetail.js
@@ -35,7 +35,7 @@ const BlogDetail = () => {
   }, [id]);
   const sendRequest = async () => {
     const res = await axios
-      .put(`http://localhost:4000/api/blog/update/${id}`, {
+      .put(`https://snapwanderer-mern.onrender.com/api/blog/update/${id}`, {
         title: inputs.title,
         description: inputs.description,
       })
